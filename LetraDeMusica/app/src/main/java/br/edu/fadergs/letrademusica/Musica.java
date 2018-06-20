8d56bd9a28c6168d521f6dec98ae385ed87877b1
HNasc <ohn.94@hotmail.com>
2018-06-20 19:27:22 -0300

    iniciando projeto LetraDeMusica

---
+++ LetraDeMusica/app/src/main/java/br/edu/fadergs/letrademusica/Musica.java
@@ -0,0 +1,43 @@
+package br.edu.fadergs.letrademusica;
+
+public class Musica {
+
+    private String nome;
+    private String artista;
+    private String letra;
+    private String letraTraduzida;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public String getLetra() {
+        return letra;
+    }
+
+    public void setLetra(String letra) {
+        this.letra = letra;
+    }
+
+    public String getLetraTraduzida() {
+        return letraTraduzida;
+    }
+
+    public void setLetraTraduzida(String letraTraduzida) {
+        this.letraTraduzida = letraTraduzida;
+    }
+
+
+}
